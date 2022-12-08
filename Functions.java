ab901467eec5542e50659716c2c8e0e161a7bd3b
Taebu <mtaebu@gmail.com>
2022-12-08 18:25:39 +0900

    Update Functions.java
    dos에서 컴파일시 public class에서 자꾸 에러

--- Functions.java
+++ Functions.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Functions extends Valiables{
+class Functions extends Valiables{
 	String get_where(String str)
 	{
 		String retVal="1";
